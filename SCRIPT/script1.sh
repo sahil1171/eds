62f92f861ab5062fd7cf00ea60be009719b0d77e
sahil1171 <98636849+sahil1171@users.noreply.github.com>
2024-03-26 15:09:09 +0530

    Update script1.sh

--- SCRIPT/script1.sh
+++ SCRIPT/script1.sh
@@ -6,4 +6,6 @@ echo "welcome to git"
 echo "this is branch-2"
 echo "this is not done yet"
 echo "something went erong "
-echo "this is merge confilct"
+echo "this is  main branch merge confilct"
+echo "this is for conflict"
+
