00f004d58fc771235eb92ebbd5090c3e4a0fc6d3
Pawar <sahil-bandu.pawar@capgemini.com>
2024-03-26 15:03:28 +0530

    this Kalpesh branch

--- SCRIPT/script1.sh
+++ SCRIPT/script1.sh
@@ -6,4 +6,5 @@ echo "welcome to git"
 echo "this is branch-2"
 echo "this is not done yet"
 echo "something went erong "
-echo "this is merge confilct"
+echo "this is merge confilct".
+echo "this is kalpesh jadhav branch"
