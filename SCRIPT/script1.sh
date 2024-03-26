19232e8de32a5e6260cf5aa0b476c361d1154a0f
Pawar <sahil-bandu.pawar@capgemini.com>
2024-03-26 15:06:07 +0530

    this main branch conflict merge

--- SCRIPT/script1.sh
+++ SCRIPT/script1.sh
@@ -3,3 +3,9 @@
 #this is 1st
 
 echo "welcome to git"
+echo "this is branch-2"
+echo "this is not done yet"
+echo "something went erong "
+echo "this is merge confilct".
+echo "this is main branch"
+echo "we are doing merge conflict"
